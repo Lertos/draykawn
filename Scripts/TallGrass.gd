e5c0064156c3525d030fa727aec6ad83aae8d4df
Lertos <deetasticgaming@gmail.com>
2023-04-12 22:05:33 -0300

    Fixing bug with grass tiles clipping above player when walking downwards and upwards

--- Scripts/TallGrass.gd
+++ Scripts/TallGrass.gd
@@ -9,3 +9,11 @@ func on_body_entered(_body):
 
 func on_body_exited(_body):
 	anim_player.play("idle")
+
+
+func on_inner_area_entered(_body):
+	$ForegroundGrass.visible = true
+
+
+func on_inner_area_exited(_body):
+	$ForegroundGrass.visible = false
